d1cb779260c83a0d21652129bf8480fdd84671d3
Cicero Romao <cicerotransformice@gmail.com>
2022-02-12 15:50:26 -0300

    ensure StormGlass Client returns a normalize forecast from the stormGlass service

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -1,12 +1,19 @@
 import { StormGlass } from '@src/clients/stormGlass'
+import stormGlassWeather3HoursFixture from '@test/fixtures/stormglass_weather_3_hours.json'
+import stormGlassNormalizedResponse3Hours from '@test/fixtures/stormglass_normalized_response_3_hours.json'
+import axios from 'axios'
+
+jest.mock('axios')
 
 describe('StormGlass client', () => {
     it('should return a normalize forecast from the stormGlass service', async () => {
         let lat = -16.000
         let lng = 152.555
 
-        const stormGlass = new StormGlass()
+        axios.get = jest.fn().mockResolvedValue({ data: stormGlassWeather3HoursFixture })
+
+        const stormGlass = new StormGlass(axios)
         const response = await stormGlass.fetchPoints(lat, lng)
-        expect(response).toEqual({})
+        expect(response).toEqual(stormGlassNormalizedResponse3Hours)
     })
 })
