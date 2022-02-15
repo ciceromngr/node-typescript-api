207850bc7182eda67e3e39820d1fbefc84c7865b
Cicero Romao <cicerotransformice@gmail.com>
2022-02-15 14:26:11 -0300

    adding headers authorization in uri stormGlass

--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -37,7 +37,13 @@ export class StormGlass {
 
     constructor(protected request: AxiosStatic) {}
     public async fetchPoints(lat: number, lng: number): Promise<ForecastPoitn[]> {
-        const response = await this.request.get<StormGlassForecastResponse>(`https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}`)
+        const response = await this.request.get<StormGlassForecastResponse>(
+            `https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}`,
+            {
+                "headers": {
+                    Authorization: 'fake-token'
+                }
+            })
         return this.normalizedResponse(response.data)
     }
 
