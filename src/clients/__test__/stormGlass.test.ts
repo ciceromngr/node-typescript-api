4dd2ca2cad8b1a8af3657a7ca4affad89ca4383e
Cicero Romao <cicerotransformice@gmail.com>
2022-02-15 14:26:55 -0300

    inference of types axios

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -6,13 +6,16 @@ import axios from 'axios'
 jest.mock('axios')
 
 describe('StormGlass client', () => {
+    
+    const mockeAxios = axios as jest.Mocked<typeof axios>
+    
     it('should return a normalize forecast from the stormGlass service', async () => {
         let lat = -16.000
         let lng = 152.555
 
-        axios.get = jest.fn().mockResolvedValue({ data: stormGlassWeather3HoursFixture })
+        mockeAxios.get.mockResolvedValue({ data: stormGlassWeather3HoursFixture })
 
-        const stormGlass = new StormGlass(axios)
+        const stormGlass = new StormGlass(mockeAxios)
         const response = await stormGlass.fetchPoints(lat, lng)
         expect(response).toEqual(stormGlassNormalizedResponse3Hours)
     })
